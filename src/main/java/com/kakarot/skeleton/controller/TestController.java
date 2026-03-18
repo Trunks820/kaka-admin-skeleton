94587b989177e1494d687e32f1f3e06a27d63c6a
KAKAROT8 <Kakarot820@outlook.com>
2026-03-18 11:09:12 +0800

    测试代码

---
+++ src/main/java/com/kakarot/skeleton/controller/TestController.java
@@ -0,0 +1,35 @@
+package com.kakarot.skeleton.controller;
+
+import com.kakarot.skeleton.common.api.ApiResponse;
+import com.kakarot.skeleton.common.exception.BizException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+//    @GetMapping("/test/ping")
+//    public ApiResponse<String> ping(){
+//        return ApiResponse.success("ping");
+//    }
+//
+//    @GetMapping("/test/biz-error")
+//    public ApiResponse<Void> bizError() {
+//        throw new BizException(4001, "这是一个业务异常测试");
+//    }
+
+
+    @GetMapping("/test/ping")
+    public String ping(){
+        return "ping";
+    }
+
+    @GetMapping("/test/biz-error")
+    public String bizError() {
+        return "这是一个业务异常测试";
+    }
+
+
+
+}
