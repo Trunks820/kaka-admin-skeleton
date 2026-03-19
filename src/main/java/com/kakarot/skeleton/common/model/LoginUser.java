3539a06341a1e041a96f5a34b9f5e2de68ff8e7e
KAKAROT8 <Kakarot820@outlook.com>
2026-03-19 17:32:29 +0800

    新增权限参数

--- src/main/java/com/kakarot/skeleton/common/model/LoginUser.java
+++ src/main/java/com/kakarot/skeleton/common/model/LoginUser.java
@@ -2,10 +2,25 @@ package com.kakarot.skeleton.common.model;
 
 import lombok.Data;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Data
 public class LoginUser {
 
     private Long userId;
     private String username;
+    private String nickName;
+
+    private Set<String> roles = new HashSet<>();
+    private Set<String> permissions = new HashSet<>();
+
+    public boolean hasRole(String roleCode){
+        return roles != null && roles.contains(roleCode);
+    }
+
+    public boolean hasPermission(String permission){
+        return permissions != null && permissions.contains(permission);
+    }
 
 }
